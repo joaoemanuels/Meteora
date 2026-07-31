4dd7a364d77c4a34617940a64287ab7629c19df2
joaoemanuels <jemanuel.pi@gmail.com>
2026-07-31 10:48:06 -0300

    fix

--- next.config.js
+++ next.config.js
@@ -1,4 +1,23 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        // GitHub Raw Content
+        protocol: "https",
+        hostname: "raw.githubusercontent.com",
+        port: "",
+        pathname: "/**", // Permite qualquer path do GitHub
+      },
+      {
+        // GitHub Assets (caso use github.com/user/repo/blob)
+        protocol: "https",
+        hostname: "github.com",
+        port: "",
+        pathname: "/**",
+      },
+    ],
+  },
+};
 
 export default nextConfig;
--- src/app/page.js
+++ src/app/page.js
@@ -1,10 +1,40 @@
 import styles from "./page.module.css";
 import { Categorias } from "./components/Categorias";
 import { Produtos } from "./components/Produtos";
+import { API_BASE_URL, API_ENDPOINTS } from "../../lib/config";
+
+const fetchCategories = async () => {
+  const response = await fetch(`${API_BASE_URL}/${API_ENDPOINTS.CATEGORIES}`, {
+    cache: "force-cache",
+  });
+  console.log(API_BASE_URL);
+  console.log(API_ENDPOINTS.CATEGORIES);
+  if (!response.ok) {
+    throw new Error(
+      `Erro ao buscar categorias: ${response.status} ${response.statusText}`,
+    );
+  }
+
+  return await response.json();
+};
+
+const fetchProducts = async () => {
+  const response = await fetch(`${API_BASE_URL}/${API_ENDPOINTS.PRODUCTS}`, {
+    cache: "no-store",
+  });
+  if (!response.ok) {
+    throw new Error(
+      `Erro ao buscar produtos ${response.status} ${response.statusText}`,
+    );
+  }
+  return await response.json();
+};
 
 export default async function Home() {
-  const categorias = [];
-  const produtos = [];
+  const [categorias, produtos] = await Promise.all([
+    fetchCategories(),
+    fetchProducts(),
+  ]);
 
   return (
     <div className={styles.page}>
